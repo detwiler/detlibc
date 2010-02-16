ea35865d824cf8f4ca54ef953e56290ebe722fac
Mike Detwiler <mike.detwiler@gmail.com>
2010-02-15 22:35:05 -0500

    use git2cl to generate ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
+echo
+echo "Generating ChangeLog..."
+
+git2cl >|ChangeLog
+
+echo
+echo "Updating gnulib sources..."
+echo
+
 gnulib-tool --update
+
+echo
+echo "Running autoreconf..."
+echo
 
 autoreconf --verbose --install --force
